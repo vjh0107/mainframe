02a98b42e39b16a18bfccca2141fc51d2cbf3c48
Junhyung Park <park@junhyung.kr>
2025-07-09 22:18:35 +0900

    (0.1.3) Add fire-and-forget coroutine scope functions

--- core/src/main/kotlin/kr/junhyung/mainframe/core/coroutines/CoroutineScopeExtensions.kt
+++ core/src/main/kotlin/kr/junhyung/mainframe/core/coroutines/CoroutineScopeExtensions.kt
@@ -2,7 +2,10 @@ package kr.junhyung.mainframe.core.coroutines
 
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.future.future
+import kotlinx.coroutines.launch
 
 public fun CoroutineScope.block(block: suspend CoroutineScope.() -> Unit) {
     this.future {
@@ -17,4 +20,14 @@ public fun CoroutineScope.block(block: suspend CoroutineScope.() -> Unit) {
             }
         }
     }.join()
+}
+
+public fun CoroutineScope.fireAndForgetScope(): CoroutineScope {
+    return CoroutineScope(coroutineContext + SupervisorJob())
+}
+
+public fun CoroutineScope.launchAndForget(
+    block: suspend CoroutineScope.() -> Unit
+): Job {
+    return fireAndForgetScope().launch(block = block)
 }
