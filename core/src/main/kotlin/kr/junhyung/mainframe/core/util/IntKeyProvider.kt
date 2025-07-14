65d4d561562a51bf06753dbbcab8f01e112cb654
Junhyung Park <park@junhyung.kr>
2025-07-15 08:48:36 +0900

    Add IntKeyProvider class for integer key management

---
+++ core/src/main/kotlin/kr/junhyung/mainframe/core/util/IntKeyProvider.kt
@@ -0,0 +1,13 @@
+package kr.junhyung.mainframe.core.util
+
+public class IntKeyProvider(override val prefix: String) : IdentifiedKeyProvider<Int> {
+
+    override fun getKey(identifier: Int): String {
+        return "$prefix$identifier"
+    }
+
+    override fun extractIdentifier(key: String): Int {
+        return key.removePrefix(prefix).toInt()
+    }
+
+}
